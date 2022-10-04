d0fc2d5aa90d43c7aac45370ed21e08c6ca4a449
JP Maia <maiajp2305@gmail.com>
2022-10-04 17:34:28 -0300

    Nilo Xcode project

---
+++ Nilo/NiloApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiloApp.swift
+//  Nilo
+//
+//  Created by Joao Pedro Monteiro Maia on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct NiloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
