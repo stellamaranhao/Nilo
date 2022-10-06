a88f7cb1bf17fbc06c8d56a57f6ee9962e1407e2
Clarathms <clarathaisms12@gmail.com>
2022-10-06 17:06:01 -0300

    API Replicate

--- Nilo/ContentView.swift
+++ Nilo/ContentView.swift
@@ -9,26 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     let api = ReplicateAPI(authToken: "c7a7ef617f35f71f74a7ce1d0ef45853d6b58cec")
+    @State var imageName = ""
+    
     
     @StateObject var networkMonitor = NetworkMonitor.shared
     
     var body: some View {
+        
+        
         VStack {
-            Text(networkMonitor.isConnected ? "Conectado" : "Não conectado")
+            AsyncImage(url: URL(string: imageName)) { image in
+                image.resizable()
+                image.scaledToFill()
+                
+            } placeholder: {
+                Image("ImageAnne")
+                    .resizable()
+            }
+            .frame(width: 250, height: 250)
+            
+            VStack {
+                Text(networkMonitor.isConnected ? "Conectado" : "Não conectado")
+            }
+            .onTapGesture{
+                api.validateToken(sucessCallback: {print("Sucesso!")}, errorCallback: {print("Erro!")})
+//               api.createPrediction(image: UIImage(imageLiteralResourceName: "ImageAnne"), sucessCallback: {print("2Sucesso2!")}, errorCallback: {print("2Erro!2")})
+            }
+            .onAppear {
+                NetworkMonitor.shared.startMonitoring()
+            }
+            .padding()
         }
-        .onTapGesture{
-            api.validateToken(sucessCallback: {print("Sucesso!")}, errorCallback: {print("Erro!")})
-//            api.createPrediction(image: UIImage(imageLiteralResourceName: "ImageAnne"), sucessCallback: {print("2Sucesso2!")}, errorCallback: {print("2Erro!2")})
-        }
-        .onAppear {
-            NetworkMonitor.shared.startMonitoring()
-        }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    func updateImageFromResponse(newURL:String){
+        self.imageName = newURL
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
