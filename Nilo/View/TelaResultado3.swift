48ada95022dc5355116c42eb0a983da830d1899e
Clarathms <clarathaisms12@gmail.com>
2022-10-26 10:51:22 -0300

    TelaResultado 95%
    + Olho funcionando
    + Botoes nos lugares e tamanhos certos
    +Pop Up ( NAO funciona direito)

--- Nilo/View/TelaResultado3.swift
+++ Nilo/View/TelaResultado3.swift
@@ -15,12 +15,16 @@ struct TelaResultado3: View {
     @Binding var imageShown:UIImage?
     @State var isClicked: Bool = false
     @State private var showingAlert = false
- //   @State var savePic: Bool = false
-
+    //   @State var savePic: Bool = false
+    @State private var showingPopover = false
+    
+    
     @State private var image : Bool = true
     @State var progressMsg:String = "progress: not started"
     @State var message:String = "GFP-GAN"
     let apicolor = ColorizeML()
+ 
+    
     
     var body: some View {
         NavigationView{
@@ -38,7 +42,7 @@ struct TelaResultado3: View {
                             .cornerRadius(15)
                             .padding(.bottom, UIScreen.main.bounds.height / 4.5)
                             .padding(.horizontal)
-                          //  .background(RoundedRectangle(cornerRadius: 15))
+                        //  .background(RoundedRectangle(cornerRadius: 15))
                     } else {
                         Image(uiImage: oldImage!)
                             .resizable()
@@ -52,10 +56,11 @@ struct TelaResultado3: View {
                         VStack {
                             NavigationLink(destination: MenuView()) {
                                 Image(systemName: "xmark.circle.fill")
-                                    .font(.system(size: 40))
-                                    .padding(.top, UIScreen.main.bounds.height / 20)
+                                    .font(.system(size: 30))
+                                    .padding(.top, UIScreen.main.bounds.height / 15)
                                     .padding(.leading)
                             }
+                            
                             Spacer()
                         }
                         Spacer()
@@ -67,7 +72,7 @@ struct TelaResultado3: View {
                             RoundedRectangle(cornerRadius: 30)
                                 .fill(Color.corDeFundo)
                                 .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 3.5)
-                            VStack(spacing: 18){
+                            VStack(spacing: 5){
                                 HStack {
                                     VStack (spacing: 5){
                                         Text("Memória Resgatada!")
@@ -83,39 +88,67 @@ struct TelaResultado3: View {
                                     }
                                     .padding(.leading, UIScreen.main.bounds.width / 4.5)
                                     Spacer()
-                                       eyeButton.frame(width: 50)
-                                        
-                                        
-                                    }
+                                    eyeButton.frame(width: 50)
+                                    
+                                    
+                                }
                                 .padding(.bottom)
                                 
                                 ScrollView(.horizontal, showsIndicators: false) {
                                     HStack(alignment: .center, spacing: 50) {
                                         
+                                        
+                                        //
+                                       
+                                        
+                                        
                                         Button {
                                             shareButton()
-                                        //    savePic = true
+                                           showingPopover = true
                                         } label: {
                                             ButtonsView(imgSistema: "square.and.arrow.up", texto: "Exportar memória")
                                         }
                                         
-                                        Button {
-                                                linkApiRestauracao()
-                                        } label: {
-                                           
-                                            ButtonsView(imgSistema: "paintbrush.pointed", texto: "Colorir memória")
-                                        }
+                                      
+                                            Button {
+                                               
+                                                if !showingPopover{
+                                                    showingPopover = true
+                                                        
+                                                }
+                                                else{linkApiRestauracao() //entra tela de carregamento
+                                                    
+                                                }
+                                            } label: {
+                                                ButtonsView(imgSistema: "paintbrush.pointed", texto: "Colorir memória")
+                                               }.alert(isPresented: $showingPopover ) {
+                                                   
+                                                Alert(title: Text("Tem certeza?")
+                                                      ,message: Text("Gostaria de salvar a foto antes de sair ou fazer outra alteração?")
+                                                      ,primaryButton: .default(Text("Salvar foto")){
+                                                    shareButton()
+                                                },secondaryButton: .destructive(Text("Continuar mesmo assim")){
+                                                    print ("foi")
+                                                    linkApiRestauracao() //entra tela de carregamento
+                                                }
+                                                      
+                                                )
+                                               }
+                                            
                                         
-                                        NavigationLink(destination: RestauracaoView()) {
-                                            ButtonsView(imgSistema: "arrow.turn.down.left", texto: "Resgatar outra memória")
-                                        }
                                         
-                                        Button {
-
-                                        } label: {
-                                            ButtonsView(imgSistema: "arrow.turn.down.left", texto: "Outra Função")
-                                        }
+                                            NavigationLink(destination: RestauracaoView()) {
+                                                ButtonsView(imgSistema: "arrow.turn.down.left", texto: "Resgatar outra memória")
+                                            }
+                                            
+                                            Button {
+                                                
+                                            } label: {
+                                                ButtonsView(imgSistema: "arrow.turn.down.left", texto: "Outra Função")
+                                            }
+                                        
                                     }.padding(.horizontal,30)
+                                    
                                 }
                             }
                         }
@@ -126,6 +159,24 @@ struct TelaResultado3: View {
         }  // Nav View
         .navigationBarBackButtonHidden(true)
     }
+    
+    var popUpAlert: some View {
+        Button("Alerta") {
+            showingPopover = true
+        }.alert(isPresented: $showingPopover ) {
+            Alert(title: Text("Tem certeza?")
+                  ,message: Text("Gostaria de salvar a foto antes de sair ou fazer outra alteração?")
+                  ,primaryButton: .default(Text("Salvar foto")){
+                shareButton()
+            },secondaryButton: .destructive(Text("Continuar mesmo assim")){
+                print ("foi")
+            }
+                  
+            )
+        }
+        
+    }
+    
     var eyeButton: some View {
         Button {
             isClicked.toggle()
@@ -135,8 +186,7 @@ struct TelaResultado3: View {
                 .font(.system(size: 40))
                 .padding(.trailing, UIScreen.main.bounds.width / 10)
         }
-
-
+        
     }
     
     func shareButton() {
@@ -177,3 +227,40 @@ struct TelaResultado3: View {
 
 
 //xmark.circle.fill
+//Button("Show Menu") {
+//                                            showingPopover = true
+//                                        }
+//                                        .popover(isPresented: $showingPopover) {
+//                                            VStack(spacing: -10){
+//                                                Image(systemName: "exclamationmark.triangle")
+//                                                    .resizable()
+//                                                    .scaledToFit()
+//                                                    .foregroundColor(Color.melhorarfoto)
+//                                                    .frame(width: UIScreen.main.bounds.width/5,height: UIScreen.main.bounds.width/5)
+//                                                    .padding(.bottom)
+//                                                Text("Tem certeza?")
+//                                                // .font(.title)
+//                                                    .font(.custom("Poppins-SemiBold", size: 26))
+//                                                    .padding()
+//                                                    .foregroundColor(Color.melhorarfoto)
+//                                                Text("Gostaria de salvar a foto antes de sair ou fazer outra alteração?")
+//                                                    .font(.custom("Poppins-Regular", size: 18))
+//                                                    .foregroundColor(Color.melhorarfoto)
+//                                                    .multilineTextAlignment(.center)
+//                                                    .padding()
+//                                               // HStack{
+//                                                    Button {
+//                                                        shareButton()
+//                                                    } label: {
+//                                                        ButtonsView(imgSistema: "square.and.arrow.up", texto: "Exportar memória")
+//                                                    }
+//                                                    Button {
+//                                                        shareButton()
+//                                                    } label: {
+//                                                        ButtonsView(imgSistema: "square.and.arrow.up", texto: "Exportar memória")
+//                                                    }
+                                           
+                                      // }
+//                                            }
+//                                        }.presentationDetents([.medium]).background(Color.corDeFundo)
+//
