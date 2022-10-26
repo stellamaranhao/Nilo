e9673c2392805d7b5ff3834dadb1b3fbb60bd2de
Clarathms <clarathaisms12@gmail.com>
2022-10-26 16:12:46 -0300

    TelaResultado pronta (sem slider)

--- Nilo/View/TelaResultado3.swift
+++ Nilo/View/TelaResultado3.swift
@@ -14,16 +14,15 @@ struct TelaResultado3: View {
     @Binding var selectedImageData: Data?
     @Binding var imageShown:UIImage?
     @State var isClicked: Bool = false
-    @State private var showingAlert = false
-    //   @State var savePic: Bool = false
-    @State private var showingPopover = false
+    @State private var shared = false
+    @State var showAlert:Bool = false
     
     
     @State private var image : Bool = true
     @State var progressMsg:String = "progress: not started"
     @State var message:String = "GFP-GAN"
     let apicolor = ColorizeML()
- 
+    
     
     
     var body: some View {
@@ -35,21 +34,24 @@ struct TelaResultado3: View {
                 
                 ZStack {
                     if (!isClicked) {
-                        Image(uiImage: imageShown!)
+                        // Image(uiImage: imageShown!)
                         // Image("ImageAnne")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .cornerRadius(15)
-                            .padding(.bottom, UIScreen.main.bounds.height / 4.5)
-                            .padding(.horizontal)
+                        ImageDetailView(image:Image(uiImage: imageShown!) )
+                        //                            .resizable()
+                        //                            .aspectRatio(contentMode: .fit)
+                        //                            .cornerRadius(15)
+                        //                            .padding(.bottom, UIScreen.main.bounds.height / 4.5)
+                        //                            .padding(.horizontal)
                         //  .background(RoundedRectangle(cornerRadius: 15))
+                        
                     } else {
-                        Image(uiImage: oldImage!)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .cornerRadius(15)
-                            .padding(.bottom, UIScreen.main.bounds.height / 4.5)
-                            .padding(.horizontal)
+                        //Image(uiImage: oldImage!)
+                        ImageDetailView(image:Image(uiImage: oldImage!) )
+                        //                            .resizable()
+                        //                            .aspectRatio(contentMode: .fit)
+                        //                            .cornerRadius(15)
+                        //                            .padding(.bottom, UIScreen.main.bounds.height / 4.5)
+                        //                            .padding(.horizontal)
                     }
                     
                     HStack {
@@ -96,56 +98,50 @@ struct TelaResultado3: View {
                                 
                                 ScrollView(.horizontal, showsIndicators: false) {
                                     HStack(alignment: .center, spacing: 50) {
-                                        
-                                        
-                                        //
-                                       
-                                        
-                                        
                                         Button {
                                             shareButton()
-                                           showingPopover = true
+                                            
                                         } label: {
                                             ButtonsView(imgSistema: "square.and.arrow.up", texto: "Exportar memória")
                                         }
                                         
-                                      
-                                            Button {
-                                               
-                                                if !showingPopover{
-                                                    showingPopover = true
-                                                        
-                                                }
-                                                else{linkApiRestauracao() //entra tela de carregamento
-                                                    
-                                                }
-                                            } label: {
-                                                ButtonsView(imgSistema: "paintbrush.pointed", texto: "Colorir memória")
-                                               }.alert(isPresented: $showingPopover ) {
-                                                   
-                                                Alert(title: Text("Tem certeza?")
-                                                      ,message: Text("Gostaria de salvar a foto antes de sair ou fazer outra alteração?")
-                                                      ,primaryButton: .default(Text("Salvar foto")){
-                                                    shareButton()
-                                                },secondaryButton: .destructive(Text("Continuar mesmo assim")){
-                                                    print ("foi")
-                                                    linkApiRestauracao() //entra tela de carregamento
-                                                }
-                                                      
-                                                )
-                                               }
-                                            
                                         
-                                        
-                                            NavigationLink(destination: RestauracaoView()) {
-                                                ButtonsView(imgSistema: "arrow.turn.down.left", texto: "Resgatar outra memória")
-                                            }
+                                        Button {
                                             
-                                            Button {
+                                            if !shared {
+                                                showAlert = true
                                                 
-                                            } label: {
-                                                ButtonsView(imgSistema: "arrow.turn.down.left", texto: "Outra Função")
+                                            } else{
+                                                linkApiRestauracao() //entra tela de carregamento
                                             }
+                                        } label: {
+                                            ButtonsView(imgSistema: "paintbrush.pointed", texto: "Colorir memória")
+                                        }
+                                        .alert(isPresented: $showAlert) {
+                                            
+                                            Alert(title: Text("Tem certeza?")
+                                                  ,message: Text("Gostaria de salvar a foto antes de sair ou fazer outra alteração?")
+                                                  ,primaryButton: .default(Text("Salvar foto")){
+                                                shareButton()
+                                            },secondaryButton: .destructive(Text("Continuar mesmo assim")){
+                                                print ("foi")
+                                                linkApiRestauracao() //entra tela de carregamento
+                                            }
+                                                  
+                                            )
+                                        }
+                                        
+                                        
+                                        
+                                        NavigationLink(destination: RestauracaoView()) {
+                                            ButtonsView(imgSistema: "arrow.turn.down.left", texto: "Resgatar outra memória")
+                                        }
+                                        
+                                        Button {
+                                            
+                                        } label: {
+                                            ButtonsView(imgSistema: "arrow.turn.down.left", texto: "Outra Função")
+                                        }
                                         
                                     }.padding(.horizontal,30)
                                     
@@ -160,22 +156,22 @@ struct TelaResultado3: View {
         .navigationBarBackButtonHidden(true)
     }
     
-    var popUpAlert: some View {
-        Button("Alerta") {
-            showingPopover = true
-        }.alert(isPresented: $showingPopover ) {
-            Alert(title: Text("Tem certeza?")
-                  ,message: Text("Gostaria de salvar a foto antes de sair ou fazer outra alteração?")
-                  ,primaryButton: .default(Text("Salvar foto")){
-                shareButton()
-            },secondaryButton: .destructive(Text("Continuar mesmo assim")){
-                print ("foi")
-            }
-                  
-            )
-        }
-        
-    }
+    //    var popUpAlert: some View {
+    //        Button("Alerta") {
+    //            showingPopover = true
+    //        }.alert(isPresented: $showingPopover ) {
+    //            Alert(title: Text("Tem certeza?")
+    //                  ,message: Text("Gostaria de salvar a foto antes de sair ou fazer outra alteração?")
+    //                  ,primaryButton: .default(Text("Salvar foto")){
+    //                shareButton()
+    //            },secondaryButton: .destructive(Text("Continuar mesmo assim")){
+    //                print ("foi")
+    //            }
+    //
+    //            )
+    //        }
+    //
+    //    }
     
     var eyeButton: some View {
         Button {
@@ -194,7 +190,8 @@ struct TelaResultado3: View {
         
         let activityController = UIActivityViewController(activityItems: [imageShown!], applicationActivities: nil)
         
-        UIApplication.shared.windows.first?.rootViewController!.present(activityController, animated: true, completion: nil)
+        UIApplication.shared.windows.first?.rootViewController!.present(activityController, animated: true, completion: {shared = true})
+        
     }
     
     func linkApiRestauracao (){
@@ -259,8 +256,8 @@ struct TelaResultado3: View {
 //                                                    } label: {
 //                                                        ButtonsView(imgSistema: "square.and.arrow.up", texto: "Exportar memória")
 //                                                    }
-                                           
-                                      // }
+
+// }
 //                                            }
 //                                        }.presentationDetents([.medium]).background(Color.corDeFundo)
 //
