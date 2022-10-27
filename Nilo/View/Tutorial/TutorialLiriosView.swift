ec445f9d60f5f716c7a4720abd05dfa1177a62ca
JP Maia <maiajp2305@gmail.com>
2022-10-27 18:44:35 -0300

    Ajuste Finos de Design

--- Nilo/View/Tutorial/TutorialLiriosView.swift
+++ Nilo/View/Tutorial/TutorialLiriosView.swift
@@ -15,16 +15,16 @@ struct TutorialLiriosView: View {
                     .foregroundColor(Color.TextoTelaTutorial)
                     .font(.custom("Poppins-Bold", size: 24))
                 
-                Text("Hora de dar vida a suas fotos! Use **LÍROS** para\n dar cor as suas memórias mais antigas.")
+                Text("Hora de dar vida a suas fotos! Use **LÍROS** para dar cor as suas memórias mais antigas.")
                     .foregroundColor(Color.TextoTelaTutorial)
-                    .font(.custom("Poppins-Regular", size: 12))
-                    .frame(maxWidth: 350)
+                    .font(.custom("Poppins-Regular", size: 14))
+                    .frame(maxWidth: UIScreen.main.bounds.width*0.85)
                     .multilineTextAlignment(.center)
                 
                 MenuItemView(imageName: "Lirio", objImageName: "ColoringTool", title: "Lirios", description: "Colorize fotos antigas")
                 
                 SliderView(imageBeforeName: "MachadoAntes",imageAferName: "MachadoDepois",
-                           containerWidth: 364, containerHeight: 250)
+                           containerWidth: 364, containerHeight: 384)
                     .cornerRadius(18)
                     .shadow(color: Color.corTexto.opacity(0.3), radius: 25, y: 8)
                 
