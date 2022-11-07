29275495e4e7ad9e42c5aa2fb7b6834d3928f30a
Clarathms <clarathaisms12@gmail.com>
2022-11-07 17:29:11 -0300

    Traduçao + Bot volta resultado final

---
+++ Nilo/View/Menu/ProcessarView.swift
@@ -0,0 +1,20 @@
+//
+//  ProcessarView.swift
+//  Nilo
+//
+//  Created by Clara Thaís Maciel e Silva on 07/11/22.
+//
+
+import SwiftUI
+
+struct ProcessarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProcessarView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProcessarView()
+    }
+}
